b02270c075521fdba7f9a093a26ba7a92300e69b
Devendork <ldevendorf@gmail.com>
2021-04-22 09:05:31 -0600

    added generated site files

---
+++ _site/assets/js/lunrsearchengine.js
@@ -0,0 +1,134 @@
+
+var documents = [{
+    "id": 0,
+    "url": "http://localhost:4000/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "http://localhost:4000/about",
+    "title": "About These Projects",
+    "body": "During the semester, each student conducted a user-centered design project that included user research, generating themes, personas, and models, wireframing and, lastely creating hi-fi prototyopes. Apps for Noticing: Our technologies can help us find new experiences in familiar spaces, perhaps even the spaces within which we have been isolated. This theme asks you how you might design applications that help people learn or notice new aspects of their everyday environments. This theme was created by Laura Devendorf. Mapping Computer Vision Applications : Computer vision technologies have been used in a variety of applications - from facial recognition to deep fakes, generated content of realistic human beings. They have also come under scrutiny for their lack of diversity, leading to apps that do not work as well on minorities, as well as the inherent danger they might pose to democracy. This theme asks you to describe current uses of computer vision technologies to the public. This theme was created by Morgan Scheuerman. Tools To Manage Our Media Diets: During the COVID-19 pandemic, many of us are bingeing comfort media (The Office, anyone?) or obsessively consuming news—or both. 2020 might be the year of the doomscroll. The media we consume influences our mood, our relationships, and our productivity. . . so why is it so hard to manage? What digital tools might help us track the media we consume — and perhaps build better habits and more balanced media diets? This theme was created by Jordan Wirfs-Brock. Difficult Data: 	How do you explain difficult concepts? How do you translate complicated data to the layperson? How do you as a designer and scientist connect with audiences? This theme asks you to create an application or technology where the input is data and the output is art. 					View our Canvas Site		More information about this course can be found on Canvas. 		Instructors		The course was led by Laura Devendorf with amazing support from Jordan Wirfs-Brock, Kandrea Wade, and Morgan Scheuerman 		 Textbooks		 We followed two textbooks this semester:		 		 	Amy Ko's Design Methods open educational text		 	Ellen Lupton's Design is Storytelling book		 		 	"
+    }, {
+    "id": 2,
+    "url": "http://localhost:4000/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "http://localhost:4000/",
+    "title": "Home",
+    "body": "      Featured:                                                                                                                                                                                                           Student Portfolio Template - HTML Embed                              :               :                                                                                                                                                                                                                                                                                  Student Portfolio Template - PDF Embed                              :                     This browser does not support PDFs. Please download the PDF to view it: Download PDF.   &lt;/embed&gt;:                                                                                                                                                                                                                                                                      Another Name                              :               link to the pdf image:                                                                    All Stories:                                                                                                     Student Portfolio Template - HTML Embed              :       :                                                                                                           The first mass-produced book to deviate from a rectilinear format              :       The first mass-produced book to deviate from a rectilinear format, at least in the United States, is thought to be this 1863 edition of Red Riding Hood, cut into the. . . :                                                                                                           Student Portfolio Template A              :       link to the pdf image:                                                                                                           Student Portfolio Template A              :       link to the pdf image:                                                                                                           Student Portfolio Template - PDF Embed              :             This browser does not support PDFs. Please download the PDF to view it: Download PDF.   &lt;/embed&gt;:                                                                                                           Student Portfolio Template A              :       link to the pdf image:                      &laquo; Prev       1        2      Next &raquo; "
+    }, {
+    "id": 4,
+    "url": "http://localhost:4000/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 5,
+    "url": "http://localhost:4000/page2/",
+    "title": "Home",
+    "body": "{% if page. url == “/” %}       Featured:       {% for post in site. posts %}    {% if post. featured == true %}      {% include featuredbox. html %}    {% endif %}  {% endfor %}  {% endif %}       All Stories:         {% for post in paginator. posts %}    {% include postbox. html %}    {% endfor %}    {% include pagination. html %}"
+    }, {
+    "id": 6,
+    "url": "http://localhost:4000/HTML/",
+    "title": "Student Portfolio Template - HTML Embed",
+    "body": "2021/04/20 - "
+    }, {
+    "id": 7,
+    "url": "http://localhost:4000/first-mass-produced/",
+    "title": "The first mass-produced book to deviate from a rectilinear format",
+    "body": "2018/06/12 - The first mass-produced book to deviate from a rectilinear format, at least in the United States, is thought to be this 1863 edition of Red Riding Hood, cut into the shape of the protagonist herself with the troublesome wolf curled at her feet. Produced by the Boston-based publisher Louis Prang, this is the first in their “Doll Series”, a set of five “die-cut” books, known also as shape books — the other titles being Robinson Crusoe, Goody Two-Shoes (also written by Red Riding Hood author Lydia Very), Cinderella, and King Winter. An 1868 Prang catalogue would later claim that such “books in the shape of a regular paper Doll… originated with us”.  It would seem the claim could also extend to die cut books in general, as we can’t find anything sooner, but do let us know in the comments if you have further light to shed on this! Such books are, of course, still popular in children’s publishing today, though the die cutting is not now limited to mere outlines, as evidenced in a beautiful 2014 version of the same Little Red Riding Hood story. The die cut has also been employed in the non-juvenile sphere as well, a recent example being Jonathan Safran Foer’s ambitious Tree of Codes. As for this particular rendition of Charles Perrault’s classic tale, the text and design is by Lydia Very (1823-1901), sister of Transcendentalist poet Jones Very. The gruesome ending of the original - which sees Little Red Riding Hood being gobbled up as well as her grandmother - is avoided here, the gore giving way to the less bloody aims of the morality tale, and the lesson that one should not disobey one’s mother. "
+    }, {
+    "id": 8,
+    "url": "http://localhost:4000/Title2/",
+    "title": "Student Portfolio Template A",
+    "body": "2018/01/11 - link to the pdf image       This browser does not support PDFs. Please download the PDF to view it: Download PDF.   &lt;/embed&gt;This is where the student’s content will go and we can add comments and share them and what not "
+    }, {
+    "id": 9,
+    "url": "http://localhost:4000/Title/",
+    "title": "Student Portfolio Template A",
+    "body": "2018/01/11 - link to the pdf image       This browser does not support PDFs. Please download the PDF to view it: Download PDF.   &lt;/embed&gt;This is where the student’s content will go and we can add comments and share them and what not "
+    }, {
+    "id": 10,
+    "url": "http://localhost:4000/PDF/",
+    "title": "Student Portfolio Template - PDF Embed",
+    "body": "2018/01/11 -       This browser does not support PDFs. Please download the PDF to view it: Download PDF.   &lt;/embed&gt;"
+    }, {
+    "id": 11,
+    "url": "http://localhost:4000/Link/",
+    "title": "Student Portfolio Template A",
+    "body": "2018/01/11 - link to the pdf image "
+    }, {
+    "id": 12,
+    "url": "http://localhost:4000/Image/",
+    "title": "Another Name",
+    "body": "2018/01/11 - link to the pdf image       This browser does not support PDFs. Please download the PDF to view it: Download PDF.   &lt;/embed&gt;This is where the student’s content will go and we can add comments and share them and what not layout: posttitle: “Can I define multiples”author: lauracategories: [ Difficult Data]tags: [iphone app, watch]image: “http://artfordorks. com/wordpress/wp-content/uploads/2021/03/WearAnimation_Web. gif”description: “This student’s project description”— "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
